2059cfcdf85d681696aab8adf08909f281a382ff
daninvngopro <danivngopro@gmail.com>
2022-11-26 09:19:50 +0200

    replacing to ts again

---
+++ App.tsx
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import registerNNPushToken from "native-notify";
+
+import WelcomePage1 from "./src/pages/welcome/WelcomePage1";
+import WelcomePage2 from "./src/pages/welcome/WelcomePage2";
+import WelcomePage3 from "./src/pages/welcome/WelcomePage3";
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  // push notifications
+  registerNNPushToken(4987, "MlbT33J7dgW3YgaI361cUu");
+  // get App ID and App Token from NativeNotify.com
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="WelcomePage1" options={{ headerShown: false }}>
+          {() => <WelcomePage1 />}
+        </Stack.Screen>
+        <Stack.Screen name="WelcomePage2" options={{ headerShown: false }}>
+          {() => <WelcomePage2/>}
+        </Stack.Screen>
+        <Stack.Screen name="WelcomePage3" options={{ headerShown: false }}>
+          {() => <WelcomePage3/>}
+        </Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
--- src/pages/welcome/WelcomePage2.tsx
+++ src/pages/welcome/WelcomePage2.tsx
@@ -1,10 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function WelcomePage2() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to start working on your app! 2</Text>
       <StatusBar style="auto" />
     </View>
   );
